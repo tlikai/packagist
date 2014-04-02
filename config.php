407d982267dede3fe937bb6c6e9246eeb0ee8dc8
wubaiqing <wubaiqing@vip.qq.com>
2014-04-03 01:49:55 +0800

    add config file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'basePath' => __DIR__,
+    'publicPath' => __DIR__ . '/public',
+    'request' => [
+        'headers' => [
+            'Accept' => 'application/json'
+        ],
+        'options' => [
+            'timeout' => 130
+        ]
+    ],
+];
