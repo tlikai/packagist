ea039464cea1e28713a1a0754589f721df2a7976
cnxh <do.not.reply@foxmail.com>
2015-08-06 21:34:30 +0800

    typo fixed

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 return [
     'basePath' => __DIR__,
     'publicPath' => __DIR__ . '/public/',
-    'dowloader' => [
+    'downloader' => [
         'options' => [
             // 'proxy' => ['127.0.0.1:3128', 'my_username', 'my_password'],
         ]
